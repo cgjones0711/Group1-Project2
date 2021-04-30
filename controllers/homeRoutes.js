199ccfb96d0117e591385ddf247eb22c6f50cefc
Zecter7523 <zanetomlinson98@gmail.com>
2021-04-30 08:47:03 -0500

    updat to comment feature

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -55,11 +55,14 @@ router.get('/recipe/:id', async (req, res) => {
           model: User,
           attributes: ['name'],
         },
+        {
+          model: Comment,
+        }
       ],
     });
 
     const recipe = recipeData.get({ plain: true });
-
+    console.log(recipe);
     res.render('recipe', {
       ...recipe,
       logged_in: req.session.logged_in
